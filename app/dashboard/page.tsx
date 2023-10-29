220356134dba12c95c019a295bdc68bb16f3c2cb
TimTimich6 <obraztsov.timo@gmail.com>
2023-10-29 12:43:36 -0700

    fix spacingt

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -20,7 +20,7 @@ export default function Build() {
   return (
     <main className="flex min-h-screen h-full  p-24 py-40 bg-primary w-full flex-col relative gap-4 items-center">
       <h1 className="text-3xl font-bold w-auto self-start">Saved Builds</h1>
-      <div className="flex flex-row  flex-wrap gap- w-full justify-between">
+      <div className="flex flex-row  flex-wrap gap- w-full justify-start gap-6">
         {listBuilds?.map((build) => (
           <Card key={build._id} build={build}></Card>
         ))}

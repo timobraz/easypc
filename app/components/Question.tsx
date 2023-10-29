aca800660877fffc3dc937ef8a2727f5d43a934e
Andrew Hwang <andrewhwang@Andrews-MacBook-Air.local>
2023-10-29 04:50:32 -0700

    animations

--- app/components/Question.tsx
+++ app/components/Question.tsx
@@ -8,6 +8,11 @@ import { useMotionValueEvent, motion, useScroll, useTransform } from "framer-mot
 
 
 export default function Question(props: any) {
+    const { scrollYProgress } = useScroll()
+const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
+  
+
+
   return (
     
     <motion.div className="flex flex-col w-full rounded-lg h-32 bg-white cursor-pointer shadow-lg shadow-gray-[350] ring-2"
@@ -16,6 +21,9 @@ export default function Question(props: any) {
       transition={{ duration: 1 }}
       whileHover={{scale: 1.05}}
       whileTap={{scale: 0.9}}
+      style={{
+        scaleY: scrollYProgress
+      }}
       
       >
       <img src={"mag.png"} className="rounded-lg" alt="" />

d4fc944563d6ff565da644359a49dae3636b46f3
jason136 <jason.wiemels@gmail.com>
2023-10-29 06:26:16 -0700

    network helpers

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,23 @@
-import { Client } from "pg";
-import dotenv from "dotenv";
-dotenv.config();
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-});
+import postgres from 'postgres'
+import crypto from 'crypto'
+
+const sql = postgres(process.env.DATABASE_URL!)
+
+async function insert_build(build: Build, use_case: string) {
+  const uuid = crypto.randomUUID();
+
+  const xs = await sql`
+    INSERT INTO public.parts (build_id, type, part, use_case)
+    VALUES
+        (${uuid}, "cpu", ${build.cpu}, ${use_case}),
+        (${uuid}, "gpu", ${build.gpu}, ${use_case}),
+        (${uuid}, "ram", ${build.ram}, ${use_case}),
+        (${uuid}, "storage", ${build.storage}, ${use_case}),
+        (${uuid}, "motherboard", ${build.motherboard}, ${use_case}),
+        (${uuid}, "psu", ${build.psu}, ${use_case}),
+    RETURNING *;
+  `
+}
+
+
+export default sql
--- lib/parser.ts
+++ lib/parser.ts
@@ -13,6 +13,8 @@ function consume(str: string, idx: number): string {
     let out_str = '';
     let count = 0;
     while (str[0 + count] != '\n') {
+        if (['<', '>', ':'].includes(str[0 + count])) continue;
+
         out_str += str[0];
         count++;
     }
@@ -20,15 +22,15 @@ function consume(str: string, idx: number): string {
 }
 
 function build_parser(str: string): Build {
-    let ram_idx = Math.max(str.toLowerCase().indexOf('ram'), str.toLowerCase().indexOf('memory'));
-    let psu_idx = Math.max(str.toLowerCase().indexOf('psu'), str.toLowerCase().indexOf('power supply'));
+    let ram_idx = Math.max(str.toLowerCase().indexOf('ram') + 3, str.toLowerCase().indexOf('memory') + 6);
+    let psu_idx = Math.max(str.toLowerCase().indexOf('psu') + 3, str.toLowerCase().indexOf('power supply') + 12);
 
     return {
-        cpu: consume(str, str.toLowerCase().indexOf('cpu')),
-        gpu: consume(str, str.toLowerCase().indexOf('gpu')),
-        ram: consume(str, ram_idx),
-        storage: consume(str, str.toLowerCase().indexOf('storage')),
-        motherboard: consume(str, str.toLowerCase().indexOf('motherboard')),
-        psu: consume(str, psu_idx),
+        cpu: consume(str, str.toLowerCase().indexOf('cpu') + 3).trim(),
+        gpu: consume(str, str.toLowerCase().indexOf('gpu') + 3).trim(),
+        ram: consume(str, ram_idx).trim(),
+        storage: consume(str, str.toLowerCase().indexOf('storage') + 7).trim(),
+        motherboard: consume(str, str.toLowerCase().indexOf('motherboard') + 11).trim(),
+        psu: consume(str, psu_idx).trim(),
     }
 }
